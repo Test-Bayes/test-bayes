b7a029edd0dec5c45ecb52929a04e256d731e065
STEVEN ALEXANDER AUSTIN <saaustin@cs.washington.edu>
2018-04-20 12:34:45 -0700

    add test metrics in discussed json format

--- src/main/java/edu/uw/cse/testbayes/Runner/IndividualClassRunner.java
+++ src/main/java/edu/uw/cse/testbayes/Runner/IndividualClassRunner.java
@@ -8,11 +8,16 @@ import org.junit.runners.model.InitializationError;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+import org.json.JSONArray;
 
 import static java.lang.System.exit;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 public class IndividualClassRunner extends BlockJUnit4ClassRunner {
     private Class<?> testClass;
@@ -54,18 +59,33 @@ public class IndividualClassRunner extends BlockJUnit4ClassRunner {
         System.out.println(methods.toString());
         for (Method method : methods) {
             if (method.isAnnotationPresent(Test.class)) {
+                Instant end = null;
+                Instant start = null;
+                boolean passed = true;
                 try {
                     System.out.println("Running test " + method.toString());
                     notifier.fireTestStarted(Description
                             .createTestDescription(testClass, method.getName()));
+                    start = Instant.now();
                     method.invoke(testObject);
+                    end = Instant.now();
                     notifier.fireTestFinished(Description
                             .createTestDescription(testClass, method.getName()));
                 } catch (InvocationTargetException e) {
+                    end = Instant.now();
+                    passed = false;
                     e.printStackTrace();
                 } catch (IllegalAccessException e) {
+                    end = Instant.now();
+                    passed = false;
                     System.out.println("Illegal test");
                     e.printStackTrace();
+                } finally {
+                    JSONArray result = new JSONArray();
+                    long time = Duration.between(start, end).toMillis();
+                    result.put(method.getName());
+                    result.put(passed ? time : (0 - time));
+                    System.out.println(result.toString());
                 }
             }
         }
