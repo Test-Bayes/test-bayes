653b4e316df0082edd1bc134649ace6e5202dbc6
STEVEN ALEXANDER AUSTIN <saaustin@cs.washington.edu>
2018-04-26 09:57:10 -0700

    add log file creation test

--- src/test/java/runner/IndividualRunnerTests.java
+++ src/test/java/runner/IndividualRunnerTests.java
@@ -1,11 +1,15 @@
 package runner;
 
 import edu.uw.cse.testbayes.Runner.IndividualClassRunner;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 import runner.utilTestClasses.Test1;
 import org.junit.Test;
 import org.junit.runners.model.InitializationError;
 
+import java.io.File;
 import java.lang.reflect.Method;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class IndividualRunnerTests {
@@ -36,5 +40,23 @@ public class IndividualRunnerTests {
                 }
             }
         }
+    }
+
+    @Test
+    // check if a log file was created within 1000ms of a run
+    public void testLogsExist() {
+        JUnitCore junit = new JUnitCore();
+        Result result = junit.run(Test1.class);
+        long timestamp = new Timestamp(System.currentTimeMillis()).getTime() ;
+        File logs = new File("log-data");
+
+        // get the minimum time difference between current time and file timestamp
+        long min = Long.MAX_VALUE;
+        for (File log: logs.listFiles()) {
+            String name = log.getName();
+            long fileTimestamp = Long.parseLong(name.split("-")[0]);
+            min = (timestamp - fileTimestamp) < min ? (timestamp - fileTimestamp): min;
+        }
+        assert(min < 1000 ? true : false);
     }
 }
