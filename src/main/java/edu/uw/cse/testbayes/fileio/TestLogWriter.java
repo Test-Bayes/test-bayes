0f27a35ee0f08a9dd71f66848a61ed82782640dd
Avidant Bhagat <avidant@cs.washington.edu>
2018-05-08 10:04:28 -0700

    Allows individual test writes

--- src/main/java/edu/uw/cse/testbayes/fileio/TestLogWriter.java
+++ src/main/java/edu/uw/cse/testbayes/fileio/TestLogWriter.java
@@ -6,8 +6,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Map;
+import java.util.Scanner;
 
 public class TestLogWriter {
+
+
+    private static String filename;
 
     /**
      * Returns the Absolute Path of the file where the given data from the test run is stored
@@ -21,22 +25,53 @@ public class TestLogWriter {
         if(testData.size() == 0) {
             return null;
         }
+        File file = getFile();
+        PrintStream printStream = new PrintStream(file);
+        for(String methodName : testData.keySet()) {
+            printStream.print(methodName + "," + testData.get(methodName) + " ");
+        }
+        printStream.println();
+        printStream.close();
+        return file.getAbsolutePath();
+    }
+
+    /**
+     * Returns the Absolute Path of the file where the given data from the test run is stored
+     *
+     * @param methodName name of the method that was tested
+     * @param num duration of the test run
+     * @return Absolute Path of file with log or null if @testData is empty
+     * @throws IOException
+     */
+    public static String write(String methodName, double num) throws IOException {
+        File file = getFile();
+        Scanner scanner = new Scanner(file);
+        String s = "";
+        if(scanner.hasNextLine()) {
+            s = scanner.nextLine();
+            System.out.println(s);
+            System.out.println();
+        }
+        PrintStream printStream = new PrintStream(file);
+        printStream.print(s + methodName + "," + num + " ");
+        printStream.close();
+        return file.getAbsolutePath();
+    }
+
+    private static File getFile() throws IOException {
         String directoryName = FileNameUtils.getDirectoryName();
         File directory = new File(directoryName);
         if(!directory.exists()) {
             directory.mkdir();
         }
-        String filename = FileNameUtils.createFileName();
+        if(filename == null) {
+            filename = FileNameUtils.createFileName();
+        }
         File file = new File(filename);
         file.createNewFile();
-        PrintStream printStream = new PrintStream(file);
-        StringBuilder stringBuilder = new StringBuilder();
-        for(String methodName : testData.keySet()) {
-            stringBuilder.append(methodName + "," + testData.get(methodName) + " ");
-        }
-        printStream.append(stringBuilder.toString().trim());
-        printStream.close();
-        return file.getAbsolutePath();
+        return file;
     }
+
+
 
 }
